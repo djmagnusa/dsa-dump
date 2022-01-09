b67b9560b0d245402a773d09936c6e73ce26a7b0
Pratush <pratush.bh@gmail.com>
2022-01-09 23:31:08 +0530

    Inorder traversal

---
+++ inorder_traversal.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    public: 
+        vector<int> inorderTraversal(TreeNode *root) {
+            stack<TreeNode*> stack; 
+            vector<int> result;
+
+            TreeNode* temp = root;
+
+            while(temp)
+            {
+                if(temp)
+                {
+                    stack.push(temp);
+                    temp=temp->left;
+                }
+
+                else
+                {
+                    temp = stack.top();
+                    stack.pop();
+                    result.push_back(temp->val);
+                    temp=temp->right;
+                }
+            }
+
+            return result;
+        }
+};
